9eaf051b2935fd7cb49c2430754b8e4abcf2b48d
Abraham <oscara1706cl@gmail.com>
2019-06-04 23:09:50 -0500

    Página la ordenamos y agregamos js

---
+++ Satnam_ingenieros_&_asociados/scripts/script.js
@@ -0,0 +1,30 @@
+
+// funcion sección conocenos
+function display_show_menu_button1(){
+  var x = document.getElementById("conocenos-firts-button");
+  var y = document.getElementById("conocenos-second-button-mision");
+  var z = document.getElementById("conocenos-third-button-servicios");
+  y.style.display = "none";
+  z.style.display = "none";
+  x.style.display = "block";
+}
+
+function display_show_menu_button2(){
+  var x = document.getElementById("conocenos-firts-button");
+  var y = document.getElementById("conocenos-second-button-mision");
+  var z = document.getElementById("conocenos-third-button-servicios");
+  x.style.display = "none";
+  z.style.display = "none";
+  y.style.display = "block";
+}
+
+function display_show_menu_button3(){
+  var x = document.getElementById("conocenos-firts-button");
+  var y = document.getElementById("conocenos-second-button-mision");
+  var z = document.getElementById("conocenos-third-button-servicios");
+  x.style.display = "none";
+  y.style.display = "none";
+  z.style.display = "block";
+}
+
+// Se caba las funciones de conocenos
