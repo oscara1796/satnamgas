67d1fa6e217defd5aaba173a4638f81511e9e871
Abraham <oscara1706cl@gmail.com>
2020-03-23 21:20:07 -0600

    Realizamos prueba dictamen tecnico mobile

---
+++ scripts/dict_tecnica.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+function display_info1(){
+  var x= document.getElementById("hidden_info_gas_lp");
+
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  }else{
+    x.style.display = "none";
+  }
+
+}
+
+function display_info2(){
+
+  var y= document.getElementById("hidden_info_gas_nat");
+
+  if (y.style.display === "none"){
+    y.style.display = "block";
+  }else{
+    y.style.display = "none";
+  }
+
+}
+
+function display_info3(){
+
+  var z= document.getElementById("hidden_info_tanque");
+
+
+  if (z.style.display === "none"){
+    z.style.display = "block";
+  }else{
+    z.style.display = "none";
+  }
+
+}
+
+// Se caba las funciones de conocenos
